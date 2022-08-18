90bce1142afc0ae7e366e2fdebc1f32bc4c0d56f
Evgeniy Polyakov <zbr@ioremap.net>
2022-08-18 02:01:26 +0100

    Changed played games

--- runner.py
+++ runner.py
@@ -24,10 +24,17 @@ class ConnectX:
         self.env = kaggle.make("connectx", debug=True, configuration={ "actTimeout": 9999999999 })
     
     def get_win_percentages(self, agent1_obj, agent2_obj, n_rounds):
-        agent1 = lambda obs, config: agent1_obj.action(obs)
-        agent2 = lambda obs, config: agent2_obj.action(obs)
+        self.logger.info(f'agent1: {agent1_obj}, agent2: {agent2_obj}, rounds: {n_rounds}')
+        def convert_agent(agent_obj):
+            if type(agent_obj) == str:
+                return agent_obj
+
+            return lambda obs, config: agent_obj.action(obs)
+
+        agent1 = convert_agent(agent1_obj)
+        agent2 = convert_agent(agent2_obj)
         
-        outcomes = kaggle.evaluate("connectx", [agent1, agent2], self.config, [], n_rounds//2, debug=True)
+        outcomes = kaggle.evaluate("connectx", [agent1, agent2], self.config, [], n_rounds//2)
         outcomes += [[b,a] for [a,b] in kaggle.evaluate("connectx", [agent2, agent1], self.config, [], n_rounds-n_rounds//2)]
 
         agent1_winning = np.round(outcomes.count([1,-1])/len(outcomes), 2)
@@ -43,8 +50,10 @@ def main():
 
     #agent1 = agents.smart_agent.Agent('smart', connect.config)
     agent2 = agents.minimax_agent.Agent('minimax', connect.config)
-    agent1 = agents.lookahead_agent.Agent('lookahead', connect.config)
-    connect.get_win_percentages(agent1, agent2, 10)
+    agent1 = 'negamax'
+    #agent1 = agents.monte_carlo_agent.Agent('monte_carlo', connect.config)
+    #agent1 = agents.lookahead_agent.Agent('lookahead', connect.config)
+    connect.get_win_percentages(agent1, agent2, 100)
 
 if __name__ == '__main__':
     main()
