6658c55917801072d5befcc78f45587425f73cbc
Evgeniy Polyakov <zbr@ioremap.net>
2022-08-18 01:59:16 +0100

    Added Monte-Carlo agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,5 @@
-import agents.smart_agent
-import agents.random_agent
 import agents.lookahead_agent
 import agents.minimax_agent
+import agents.monte_carlo_agent
+import agents.random_agent
+import agents.smart_agent
