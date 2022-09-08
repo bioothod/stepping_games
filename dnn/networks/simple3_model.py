4f67240f705f66437d13a81570efa4a68f1d50d3
Evgeniy Polyakov <zbr@ioremap.net>
2022-09-08 17:56:33 +0100

    Network update

--- dnn/networks/simple3_model.py
+++ dnn/networks/simple3_model.py
@@ -12,17 +12,41 @@ class Model(nn.Module):
 
         self.config = config
 
-        num_output_conv_features = 128
-        num_input_linear_features = num_output_conv_features * self.config.rows * self.config.columns
+        num_output_conv_features = 512
+        num_input_linear_features = num_output_conv_features * (self.config.rows - 3) * self.config.columns
 
         self.conv_encoder = nn.Sequential(
             nn.Conv2d(1, 2, 1),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(2),
 
-            nn.Conv2d(2, 128, 4, padding='same', padding_mode='zeros'),
+            nn.Conv2d(2, 16, 3, padding='same', padding_mode='zeros'),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(16),
+
+            nn.Conv2d(16, 32, 3, padding='same', padding_mode='zeros'),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+
+            nn.Conv2d(32, 64, 3, padding='same', padding_mode='zeros'),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+
+            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=(1, 0)),
+
+            nn.Conv2d(64, 128, 3, padding='same', padding_mode='zeros'),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(128),
+
+            nn.Conv2d(128, 256, 3, padding='same', padding_mode='zeros'),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+
+            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=(1, 0)),
+
+            nn.Conv2d(256, 512, 3, padding='same', padding_mode='zeros'),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
         )
         
         self.linear_encoder = nn.Sequential(
@@ -36,5 +60,5 @@ class Model(nn.Module):
     def forward(self, inputs):
         conv_features = self.conv_encoder(inputs)
         linear_features = self.linear_encoder(conv_features)
-        
+
         return linear_features
