68181ede406abb8915579bb9822203519f508c2c
Evgeniy Polyakov <zbr@ioremap.net>
2022-09-20 01:24:43 +0100

    Removed unused imports

--- dnn/networks/simple3_model.py
+++ dnn/networks/simple3_model.py
@@ -2,9 +2,6 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-from .conv_model import ConvBlock
 
 class Model(nn.Module):
     def __init__(self, config):
