1a4eabe395d68f4b48c495aa063aeacea34edb92
Evgeniy Polyakov <zbr@ioremap.net>
2022-09-29 05:15:54 +0100

    Different architecture saved into simple2_model.py

--- dnn/networks/simple2_model.py
+++ dnn/networks/simple2_model.py
@@ -3,40 +3,69 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+class ConvBlockSingle(nn.Module):
+    def __init__(self, num_inputs, num_outputs, kernel_size=3):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(num_inputs, num_outputs, kernel_size, padding='same', padding_mode='zeros'),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(num_outputs),
+        )
+
+    def forward(self, inputs):
+        return self.model(inputs)
+
+class ConvBlockDouble(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            ConvBlockSingle(num_features, num_features),
+            ConvBlockSingle(num_features, num_features),
+        )
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        #x += inputs
+        return x
+
 class Model(nn.Module):
     def __init__(self, config):
         super().__init__()
 
         self.config = config
 
-        num_output_conv_features = 64
-        num_input_linear_features = num_output_conv_features * self.config.rows * self.config.columns
-        num_linear_features = 4096
+        num_output_conv_features = 1024
+        num_input_linear_features = num_output_conv_features * (self.config.rows - 3) * self.config.columns
 
         self.conv_encoder = nn.Sequential(
-            nn.Conv2d(1, 2, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(2),
-            
-            nn.Conv2d(2, 64, 4, padding='same', padding_mode='zeros'),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(64),
+            ConvBlockSingle(1, 3, 1),
+            ConvBlockSingle(3, 64),
+
+            ConvBlockSingle(64, 128),
+
+            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=(1, 0)),
+
+            ConvBlockSingle(128, 256),
+
+            ConvBlockSingle(256, 512),
+
+            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1), padding=(1, 0)),
+
+            ConvBlockSingle(512, 1024),
         )
         
         self.linear_encoder = nn.Sequential(
             nn.Flatten(),
         
-            nn.Linear(num_input_linear_features, num_linear_features),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-
-            nn.Linear(num_linear_features, self.config.num_features),
-            nn.ReLU(),
+            nn.Linear(num_input_linear_features, self.config.num_features),
+            nn.ReLU(inplace=True),
             nn.BatchNorm1d(self.config.num_features),
         )
 
     def forward(self, inputs):
         conv_features = self.conv_encoder(inputs)
         linear_features = self.linear_encoder(conv_features)
-        
+
         return linear_features
