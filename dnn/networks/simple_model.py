c8c2864d7e62cd0e7a89a3b60a8ae607630e5cc6
Evgeniy Polyakov <zbr@ioremap.net>
2022-10-09 13:56:30 +0100

    simple_model: this version doesn't work well

--- dnn/networks/simple_model.py
+++ dnn/networks/simple_model.py
@@ -10,20 +10,21 @@ class Model(nn.Module):
         self.config = config
         self.num_actions = self.config.columns
 
-        num_output_conv_features = 1
-        num_input_linear_features = num_output_conv_features * self.config.rows * self.config.columns
-        num_linear_features = num_output_conv_features * self.config.rows * self.config.columns * 10
-        
+        num_output_conv_features = 128
+        num_input_linear_features = num_output_conv_features * (self.config.rows) * (self.config.columns)
+
         self.conv_encoder = nn.Sequential(
-            nn.BatchNorm2d(1),
-            
-            nn.Conv2d(1, 16, 4, padding='same', padding_mode='zeros'),
+            nn.Conv2d(1, 3, 1, padding='same', padding_mode='zeros'),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
+            nn.BatchNorm2d(3),
             
-            nn.Conv2d(16, num_output_conv_features, 4, padding='same', padding_mode='zeros'),
+            nn.Conv2d(3, 128, 4, padding='same', padding_mode='zeros'),
             nn.ReLU(),
-            nn.BatchNorm2d(num_output_conv_features),
+            nn.BatchNorm2d(128),
+
+            nn.Conv2d(128, 128, 4, padding='same', padding_mode='zeros'),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
         )
 
         class Debug(nn.Module):
@@ -35,46 +36,15 @@ class Model(nn.Module):
                 return x
         
         self.linear_encoder = nn.Sequential(
-            nn.BatchNorm1d(num_input_linear_features),
+            nn.Flatten(),
 
-            nn.Linear(num_input_linear_features, num_linear_features),
+            nn.Linear(num_input_linear_features, self.config.num_features),
             nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-            
-            nn.Linear(num_linear_features, num_linear_features),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-
-            nn.Linear(num_linear_features, num_linear_features),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-
-            nn.Linear(num_linear_features, num_linear_features),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-
-            nn.Linear(num_linear_features, num_linear_features),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-
-            nn.Linear(num_linear_features, num_linear_features),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-
-            nn.Linear(num_linear_features, num_linear_features),
-            nn.ReLU(),
-            nn.BatchNorm1d(num_linear_features),
-
-            nn.Linear(num_linear_features, self.config.num_features),
-            nn.ReLU(),
+            nn.BatchNorm1d(self.config.num_features),
         )
 
     def forward(self, inputs):
-        #conv_features = self.conv_encoder(inputs)
-        conv_features = inputs
-        batch_size, channels = conv_features.shape[:2]
-        features = torch.reshape(conv_features, [batch_size, -1])
-
-        linear_features = self.linear_encoder(features)
+        conv_features = self.conv_encoder(inputs)
+        linear_features = self.linear_encoder(conv_features)
         
         return linear_features
