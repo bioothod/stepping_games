b73926a561199b3d17cdf7a3be44d9a4b0731a68
Evgeniy Polyakov <zbr@ioremap.net>
2022-10-24 13:51:47 +0100

    Added option to save critic's weights

--- dnn/submission/convert_checkpoint.py
+++ dnn/submission/convert_checkpoint.py
@@ -3,16 +3,21 @@ import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--original', type=str, required=True, help='Original checkpoint')
+parser.add_argument('--want_critic', action='store_true', help='Whether to store critic weights')
 parser.add_argument('--output', type=str, required=True, help='Output checkpoint path')
 FLAGS = parser.parse_args()
 
 def main():
     checkpoint = torch.load(FLAGS.original)
 
-    torch.save({
+    output_dict = {
         'actor_state_dict': checkpoint['actor_state_dict'],
-        'critic_state_dict': checkpoint['critic_state_dict'],
-    }, FLAGS.output)
+    }
+    if FLAGS.want_critic:
+        output_dict['critic_state_dict'] = checkpoint['critic_state_dict']
+
+    torch.save(output_dict, FLAGS.output)
+    print(f'saved to {FLAGS.output}')
 
 if __name__ == '__main__':
     main()
