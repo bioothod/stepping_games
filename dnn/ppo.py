a951e73a946c3cc2ddfbecd9a67944748237ad73
Evgeniy Polyakov <zbr@ioremap.net>
2022-11-07 16:05:20 +0300

    Cleaned up, removed unused play-against-the-best-self mode

--- dnn/ppo.py
+++ dnn/ppo.py
@@ -22,7 +22,7 @@ class PPO(train_selfplay.BaseTrainer):
             'player_ids': [1, 2],
             'train_player_id': 1,
 
-            'checkpoints_dir': 'checkpoints_ppo_22',
+            'checkpoints_dir': 'checkpoints_ppo_24',
             'eval_agent_template': 'submission/feature_model_ppo6.py:submission/rl_agents_ppo6.py:checkpoints_simple3_ppo_6/ppo_100.ckpt',
             'score_evaluation_dataset': 'refmoves1k_kaggle',
 
@@ -31,12 +31,12 @@ class PPO(train_selfplay.BaseTrainer):
             'max_episode_len': 42,
 
             'policy_optimization_steps': 10,
-            'policy_clip_range': 0.03,
+            'policy_clip_range': 0.1,
             'policy_stopping_kl': 0.02,
 
             'value_optimization_steps': 10,
             'value_clip_range': float('inf'),
-            'value_stopping_mse': 0.86,
+            'value_stopping_mse': 0.85,
 
             'entropy_loss_weight': 0.1,
 
@@ -48,20 +48,6 @@ class PPO(train_selfplay.BaseTrainer):
 
             'init_lr': 1e-5,
 
-            'num_games_to_stop_training_state_model': 100_000_000,
-
-            'channels': 3,
-            'num_layers': 4,
-            'hidden_size': 512,
-            'filter_size': 1024,
-            'decoder_dropout': 0.,
-            'total_key_depth': 64,
-            'total_value_depth': 64,
-            'attn_num_heads': 2,
-            'attn_type': 'global',
-            'distr': 'cat',
-
-
             'num_features': 512,
             'hidden_dims': [128],
 
@@ -69,7 +55,7 @@ class PPO(train_selfplay.BaseTrainer):
 
             'num_training_games': 1024*2,
 
-            'batch_size': 1024*8,
+            'batch_size': 1024*12,
             'experience_buffer_to_batch_size_ratio': 2,
         })
 
@@ -363,71 +349,12 @@ class PPO(train_selfplay.BaseTrainer):
 
         return True
 
-    def make_indexes(self, game_states, agents):
-        index_all = torch.arange(len(game_states))
-        middle_point = len(game_states) // 2
-        index0 = index_all[:middle_point]
-        index1 = index_all[middle_point:]
-
-        ret_indexes = [index0, index1]
-        ret_agents = [(agents[0], agents[1]), (agents[1], agents[0])]
-
-        return ret_indexes, ret_agents
-
-    def make_values(self, player_id, game_states):
-        indexes, agents = self.make_indexes(game_states, [self.critic, self.best_critic])
-
-        all_values = []
-
-        player_index = self.config.player_ids.index(player_id)
-
-        for index, agents in zip(indexes, agents):
-            play_agent = agents[player_index]
-
-            play_states = game_states[index]
-            with torch.no_grad():
-                values = play_agent(play_states)
-
-            all_values.append(values)
-
-        all_values = torch.cat(all_values, 0)
-
-        return all_values
-
-    def make_actions(self, player_id, game_states):
-        indexes, agents = self.make_indexes(game_states, [self.actor, self.best_actor])
-
-        all_actions = []
-        all_log_probs = []
-        all_explorations = []
-
-        player_index = self.config.player_ids.index(player_id)
-
-        for index, agents in zip(indexes, agents):
-            play_agent = agents[player_index]
-
-            play_states = game_states[index]
-            with torch.no_grad():
-                actions, log_probs, explorations = play_agent.dist_actions(player_id, play_states)
-
-            all_actions.append(actions)
-            all_log_probs.append(log_probs)
-            all_explorations.append(explorations)
-
-        all_actions = torch.cat(all_actions, 0)
-        all_log_probs = torch.cat(all_log_probs, 0)
-        all_explorations = torch.cat(all_explorations, 0)
-
-        return all_actions, all_log_probs, all_explorations
-
-
     def make_single_step_and_save(self, player_id):
         game_index, game_states = self.train_env.current_states()
         if len(game_index) == 0:
             return
 
         with torch.no_grad():
-            #actions, log_probs, explorations = self.make_actions(player_id, game_states)
             actions, log_probs, explorations = self.actor.dist_actions(player_id, game_states)
 
         new_states, rewards, dones = self.train_env.step(player_id, game_index, actions)
@@ -450,14 +377,6 @@ class PPO(train_selfplay.BaseTrainer):
     def make_step(self):
         for player_id in self.config.player_ids:
             self.make_single_step_and_save(player_id)
-
-        if self.train_env.total_games_completed > self.config.num_games_to_stop_training_state_model:
-            if self.actor.train_state_features:
-                self.logger.info(f'completed_games: {self.train_env.total_games_completed}, '
-                                 f'num_games_to_stop_training_state_model: {self.config.num_games_to_stop_training_state_model}: '
-                                 f'finishing training the state model')
-
-            self.actor.train_state_features = False
 
     def fill_episode_buffer(self):
         self.train_env.reset()
