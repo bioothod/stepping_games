2b49dd0bc50140e5939f0c831a0d9d082268f766
Evgeniy Polyakov <zbr@ioremap.net>
2022-12-02 23:16:44 +0000

    Use a new feature extraction model

--- dnn/ppo.py
+++ dnn/ppo.py
@@ -25,7 +25,7 @@ class PPO(train_selfplay.BaseTrainer):
             'player_ids': [1, 2],
             'train_player_id': 1,
 
-            'checkpoints_dir': 'checkpoints_ppo_27',
+            'checkpoints_dir': 'checkpoints_ppo_28',
             'eval_agent_template': 'submission/feature_model_ppo6.py:submission/rl_agents_ppo6.py:checkpoints_simple3_ppo_6/ppo_100.ckpt',
             'score_evaluation_dataset': 'refmoves1k_kaggle',
 
@@ -45,20 +45,20 @@ class PPO(train_selfplay.BaseTrainer):
 
             'weight_decay': 1e-3,
 
-            'gamma': 0.999,
+            'gamma': 1.0,
             'tau': 0.97,
             'default_reward': 0,
 
             'init_lr': 1e-5,
 
             'num_features': 512,
-            'hidden_dims': [256],
+            'hidden_dims': [128],
 
             'max_gradient_norm': 1.0,
 
             'num_training_games': 1024*3,
 
-            'batch_size': 1024*16,
+            'batch_size': 1024*32,
             'experience_buffer_to_batch_size_ratio': 2,
             'train_break_after_num_sampled': 1,
         })
@@ -74,7 +74,7 @@ class PPO(train_selfplay.BaseTrainer):
         self.name = 'ppo'
 
         def feature_model_creation_func(config):
-            model = networks.simple3_model.Model(config)
+            model = networks.simple4_model.Model(config)
             return model
 
 
